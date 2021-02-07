7f3f9175d5725789cc3231f9050dc7233511693b
Amandeep Bhatia <amanbhatia.canada@gmail.com>
2021-02-06 19:30:20 -0500

    Updates

---
+++ precache-manifest.da92f9a9298337fb3d75a3a7649c3fac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "942d8f7267a30361e85b9bc1635b310a",
+    "url": "/birthday/index.html"
+  },
+  {
+    "revision": "8427828e865f5111c9f9",
+    "url": "/birthday/static/css/main.750b1691.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/birthday/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/birthday/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8427828e865f5111c9f9",
+    "url": "/birthday/static/js/main.9624cfc6.chunk.js"
+  },
+  {
+    "revision": "3db92290b359abe36f89",
+    "url": "/birthday/static/js/runtime-main.d025061d.js"
+  }
+]);
